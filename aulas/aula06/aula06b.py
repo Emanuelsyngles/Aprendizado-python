c62abfaa3c1a7d0958472d94e004705c0afd4c83
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-09-08 20:10:31 -0300

    Novos métodos.

---
+++ aulas/aula06/aula06b.py
@@ -0,0 +1,8 @@
+n = input('Digite um valor: ')
+
+print(n.isalnum())
+
+#print(n.isnumeric()) - Ele vai dizer se é possivel converter esse valor em um número com o tipo primitivo 'int'.
+
+#print(n.isalpha) - Esse verifica se ele é letra, se digitarmos letras ele retornará true, agora se digitarmos por exemplo: 3a ele retornará false.
+
